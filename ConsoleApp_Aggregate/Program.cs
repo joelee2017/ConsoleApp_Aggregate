5c7623d9a880fda725bbaea4f36b6b1d3091d935
joe lee <as955133@gmail.com>
2018-06-06 23:09:28 +0800

    Aggregate() LINQ

--- ConsoleApp_Aggregate/Program.cs
+++ ConsoleApp_Aggregate/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp_Aggregate
     {
         static void Main(string[] args)
         {
+            //存款 
+            double myBalance = 100.0;
+
+            //提款的額度
+            int[] withdrawItems = { 20, 10, 40, 50, 10, 70, 30 };
+
+            double balance = withdrawItems.Aggregate(myBalance, (originbalance, nextWithdrawal) => 
+            {
+                Console.WriteLine("originbalance: {0}, nextWithdrawal: {1}",
+                    originbalance, nextWithdrawal);
+                Console.WriteLine("Withdrawal status: {0}", (nextWithdrawal <= originbalance) ? "OK" : "FAILED");
+
+                //若存款餘額不夠時，不會扣除，否則扣除提款額度。
+                return ((nextWithdrawal <= originbalance) ? (originbalance - nextWithdrawal) : originbalance);
+            });
+
+            //顯示最終的存款數
+            Console.WriteLine("Ending balance: {0}", balance);
+            Console.Read();
         }
     }
 }
